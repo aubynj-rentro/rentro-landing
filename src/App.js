2d1c34436d4c26edcb891666fc377be50ad9ac60
aubynj <john.aubynj@gmail.com>
2020-05-29 09:26:52 +0000

    Bootstrapping project with Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/NavbarComponent';
+import FooterComponent from './Components/FooterComponent';
+import './assets/css/custom.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+            <Navbar />
+            {/* <div className="main-intro">
+
+            </div> */}
+
+            {/* <div className="mt-10">
+                <h2 className="text-4xl text-center text-gray-800"> Website I have build</h2><br/>
+                <div className="container flex flex-wrap push-up sm:mt-auto" id="websites-details">
+
+                </div>
+                <div className="bg-gray-900">
+                    <h2 className="text-4xl p-10 text-center text-gray-200">GitHub Projects Samples</h2><br/>
+                    <div className="container flex flex-wrap push-up">
+                    </div>
+                    <div className="text-center p-10">
+                    </div>
+                </div>
+                <FooterComponent />
+            </div> */}
+      </div>
   );
 }
 
---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+    return (
+        <div className="bg-black p-5">
+            <div className="flex item-center lg:justify-between text-white px-5">
+                
+            </div>
+        </div>
+    )
+}
+
+export default FooterComponent
